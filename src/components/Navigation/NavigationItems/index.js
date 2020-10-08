2935754d4bf6a1f10e1162773c8b849cdb047e60
Marco Echevestre <marcoaminotto@hotmail.com>
2020-10-08 13:44:25 +0200

    Create Navigation component

---
+++ src/components/Navigation/NavigationItems/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import NavigationItem from './NavigationItem';
+
+import classes from './Styles.module.css';
+
+const navigationItems = () => (
+  <ul className={classes.NavigationItems}>
+    <NavigationItem link="/" active>
+      Burger Builder
+    </NavigationItem>
+    <NavigationItem link="/">
+      Checkout
+    </NavigationItem>
+  </ul>
+);
+
+export default navigationItems;
