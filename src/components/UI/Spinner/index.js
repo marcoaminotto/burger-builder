fc34b91eab2c87fc3f5736fae6b933d27fa314d6
Marco Echevestre <marcoaminotto@hotmail.com>
2020-10-10 12:53:50 +0200

    Fix the code

--- src/components/UI/Spinner/index.js
+++ src/components/UI/Spinner/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 import classes from './Styles.module.css';
 
-const spinner = () => <div class={classes.Loader}>Loading...</div>;
+const spinner = () => <div className={classes.Loader}>Loading...</div>;
 
 export default spinner;
